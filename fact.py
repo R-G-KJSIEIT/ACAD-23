1d2aa0a79fc193b4db265c8b50c425aef3415b32
Rishikesh <110241863+R-G-KJSIEIT@users.noreply.github.com>
2023-01-10 19:54:51 +0530

    Update fact.py

--- fact.py
+++ fact.py
@@ -1,8 +1,11 @@
-factorial = f
-n = int (input ("Enter a number to find it's factorial"))
-if n == 0:
-    print("Error")
-else:
-    while n>=1:
-        n=n*f
-        n=n-1
+def factorial(n):
+    if n < 0:
+        return "Invalid Input"
+    elif n == 0:
+        return 1
+    else:
+        fact = 1
+        while n > 0:
+            fact = fact * n
+            n = n - 1
+        return fact
