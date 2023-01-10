41c2ac5fc4a02071b7d1bc63a193f2762060a27c
Rishikesh <110241863+R-G-KJSIEIT@users.noreply.github.com>
2023-01-10 19:47:41 +0530

    10-1

---
+++ fact.py
@@ -0,0 +1,8 @@
+factorial = f
+n = int (input ("Enter a number to find it's factorial"))
+if n == 0:
+    print("Error")
+else:
+    while n>=1:
+        n=n*f
+        n=n-1
---
+++ units.py
@@ -0,0 +1,13 @@
+disk =int (input ("Enter the distance between the two cities (Km):    "))
+
+print ("Distance in m:")
+print (disk * 1000)
+
+print ("Distance in feet:")
+print (disk * 3280.8399)
+
+print ("Distance in inches:")
+print (disk * 39370.0787)
+
+print ("Distance in centimeters:")
+print (disk * 100000)
