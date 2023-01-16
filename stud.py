58dae70f5b24ee488b5999fcbdf7f78f808fe51a
Rishikesh <110241863+R-G-KJSIEIT@users.noreply.github.com>
2023-01-16 12:37:37 +0530

    Update stud.py

--- stud.py
+++ stud.py
@@ -1,14 +1,19 @@
-def student(s,m):
-    student_name = s
-    student_marks = m
-    print("Student name is: "+s+"    Student marks is:  "+m)
+class Studentt:
+    student_name = "Rishi"
+    student_marks = 98
+    def student(s,m):
+        student_name = s
+        student_marks = m
 
-print ("Original Name and Marks: ")
-s="Rishi"
-m="97"
-student(s,m)
-print("Enter student name and marks")
-s = input()
-m = input()
-student(s,m)
+stud=Studentt()
 
+print(stud.student_name)
+print(stud.student_marks)
+
+s = input("Enter Student name:   ")
+m = input("Enter Student marks:  ")
+stud.student_name = s
+stud.student_marks = m
+
+print(stud.student_name)
+print(stud.student_marks)
