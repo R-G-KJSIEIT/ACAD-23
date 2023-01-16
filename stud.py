4e20e877c90b65800fac5161a3935fc3dba745ea
Rishikesh <110241863+R-G-KJSIEIT@users.noreply.github.com>
2023-01-16 12:13:50 +0530

    Update stud.py

--- stud.py
+++ stud.py
@@ -3,6 +3,10 @@ def student(s,m):
     student_marks = m
     print("Student name is: "+s+"    Student marks is:  "+m)
 
+print ("Original Name and Marks: ")
+s="Rishi"
+m="97"
+student(s,m)
 print("Enter student name and marks")
 s = input()
 m = input()
