551e64ff14d10574a09608ae329c695de72f4723
Rishikesh <110241863+R-G-KJSIT@users.noreply.github.com>
2023-03-27 23:25:19 +0530

    Update mm.c

--- A13/A13-e8/mm.c
+++ A13/A13-e8/mm.c
@@ -2,41 +2,33 @@
 
 int main()
 {
-	int osm,tm,tif,i,j,n,p[n],b,nb,inf[n];
-	printf("This program has been created by Rishikesh Giridhar");
-	printf("\nEnter total available Memory:");
-	scanf("%d",&tm);
-	printf("\nEnter amount of memory occupied by the OS: ");
-	scanf("%d",&osm);
-	printf("\nEnter number of processes: ");
-	scanf("%d",&n);
-	printf("\nEnter size of each block: ");
-	scanf("%d",&b);
-	nb=tm/b;
-	tm-=osm;
-	for(i=0;i<=n;i++) {
-		printf("\nEnter required memory for process %d: ",i);
-		scanf("%d",&p[i]);
-	}
-	for (i=0;i<=n;i++) {
-		inf[i]=b-p[i];
-		printf("\nprocess %d: \nmemory required:%d \n remaining memory:%d \n internal fragmentation:%d",i,p[i],tm,inf[i]);
-		tm-=p[i];
-	}
-	for (i=0;i<=n;i++) {
-		if (p[i]<b) {
-			inf[i]=tm-p[i];
-		}
-		else if(p[i]==b) {
-			continue;
-		}
-		else {
-			printf("cannot allocate sorry");
-		}
-	}
-	for (i=0;i<=n;i++) {
-		tif+=inf[i];
-	}
-	printf("\nTotal internal fragmentation is:%d",tif);
-	return 0;
+    int osm, tm, tif = 0, i, n, b, nb;
+    printf("This program has been created by Rishikesh Giridhar");
+    printf("\nEnter total available Memory:");
+    scanf("%d", &tm);
+    printf("\nEnter amount of memory occupied by the OS: ");
+    scanf("%d", &osm);
+    tm -= osm;
+    printf("\nEnter number of processes: ");
+    scanf("%d", &n);
+    printf("\nEnter size of each block: ");
+    scanf("%d", &b);
+    nb = tm / b;
+
+    int p[n], inf[n];
+    for (i = 0; i < n; i++) {
+        printf("\nEnter required memory for process %d: ", i);
+        scanf("%d", &p[i]);
+        if (p[i] > b) {
+            printf("Process %d cannot be allocated. Sorry.\n", i);
+            return 1;
+        }
+        inf[i] = b - p[i];
+        printf("process %d: \nmemory required:%d \nremaining memory:%d \ninternal fragmentation:%d\n", i, p[i], tm, inf[i]);
+        tif += inf[i];
+        tm -= p[i];
+    }
+
+    printf("\nTotal internal fragmentation is:%d\n", tif);
+    return 0;
 }
