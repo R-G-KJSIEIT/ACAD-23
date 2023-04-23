b06c9d87429a55da8b38df43c0ed5565a2efdaa5
Rishikesh <110241863+R-G-KJSIT@users.noreply.github.com>
2023-04-23 23:23:42 +0530

    Update AAR_exp2.c

--- AAR_exp2.c
+++ AAR_exp2.c
@@ -1,28 +1,27 @@
-#include <stdio.h>
+#include<stdio.h>
+#include<stdlib.h>
+#include<math.h>
+
 int main()
 {
-    int ar[100],n,i,j,tmp;
-    printf("Enter the number of elements in array:\t");
-    scanf("%d",&n);
-    printf("Enter the array elements:\t");
-    for (i=0;i<n;i++)
-    {
-        scanf("%d",&ar[i]);
-    }
-    for (i=1;i<n;i++)
-    {
-        for (j=i;j>0 && ar[j-1]>ar[j];j--)
-        {
-            tmp=ar[j];
-            ar[j]=ar[j-1];
-            ar[j-1]=tmp;
-        }
-    }
-    printf("Result is:\n");
-    for (i=0;i<n;i++)
-    {
-        printf("%d\t",ar[i]);
-    }
-    printf("\n");
-    return 0;
+	int i,j,key;
+	printf("yes yes yes\n");
+	int arr[] = {24,23,25,27,26};
+	int n  = sizeof(arr)/sizeof(arr[0]);
+	for (i=1;i<n;i++)
+	{
+		key=arr[i];
+		j=i-1;
+		while (j>=0 && arr[j]>key) 
+		{
+			arr[j+1] = arr[j];
+			j = j-1;
+		}
+		arr[j+1]=key;
+	}
+	for (i=0;i<n;i++)
+	{
+		printf("%d\t",arr[i]);
+	}
+	return 0;
 }
